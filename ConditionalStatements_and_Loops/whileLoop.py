434a7b83bee7e738364b6213c05ab99bad46777f
Mustafa Ozen <17510066+mustafaozen@users.noreply.github.com>
2020-09-13 15:58:31 -0400

    Update whileLoop.py

--- ConditionalStatements_and_Loops/whileLoop.py
+++ ConditionalStatements_and_Loops/whileLoop.py
@@ -2,7 +2,7 @@
 This script exemplifies basic use of while loops in python.
 '''
 
-# while loops iterate until a condition is satisfied.
+# while loops iterate as long as a condition is satisfied.
 index = 0
 while(index < 5):
     print('index: ', index)
